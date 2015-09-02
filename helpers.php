8bdb481994cec6d15f698bd3f06560c53c620990
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-02 17:27:09 +0100

    Adding more helpers

--- helpers.php
+++ helpers.php
@@ -10,3 +10,29 @@ if ( ! function_exists('log_viewer')) {
         return app('log-viewer');
     }
 }
+
+if ( ! function_exists('extract_date')) {
+    /**
+     * Extract date from string
+     *
+     * @param  string  $string
+     *
+     * @return string
+     */
+    function extract_date($string) {
+        return preg_replace('/.*(' . REGEX_DATE_PATTERN . ').*/', '$1', $string);
+    }
+}
+
+if ( ! function_exists('extract_datetime')) {
+    /**
+     * Extract date from string
+     *
+     * @param  string  $string
+     *
+     * @return string
+     */
+    function extract_datetime($string) {
+        return preg_replace('/.*(' . REGEX_DATETIME_PATTERN . ').*/', '$1', $string);
+    }
+}
