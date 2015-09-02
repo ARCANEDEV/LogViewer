662f3c83077160e2724a1aa8d06fc7f5ad2f6fc6
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-02 17:28:03 +0100

    Updating the base TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,11 @@
 <?php namespace Arcanedev\LogViewer\Tests;
 
-use Arcanedev\LogViewer\Entities\EntryCollection;
+use Arcanedev\LogViewer\Entities\LogEntryCollection;
+use Arcanedev\LogViewer\Entities\Log;
 use Arcanedev\LogViewer\Entities\LogEntry;
 use Arcanedev\LogViewer\LogViewerServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Foundation\Application;
 use Psr\Log\LogLevel;
 use ReflectionClass;
 
@@ -14,13 +16,38 @@ use ReflectionClass;
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var array */
+    protected static $logLevels;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public static function setUpBeforeClass()
+    {
+        parent::setUpBeforeClass();
+
+        static::$logLevels = self::getLogLevels();
+    }
+
+    public static function tearDownAfterClass()
+    {
+        parent::tearDownAfterClass();
+
+        static::$logLevels = [];
+    }
+
+    /* ------------------------------------------------------------------------------------------------
      |  Bench Functions
      | ------------------------------------------------------------------------------------------------
      */
     /**
      * Get package providers.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param  Application  $app
      *
      * @return array
      */
@@ -34,7 +61,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Get package aliases.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param  Application  $app
      *
      * @return array
      */
@@ -47,7 +74,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Define environment setup.
      *
-     * @param  \Illuminate\Foundation\Application  $app
+     * @param  Application  $app
      */
     protected function getEnvironmentSetUp($app)
     {
@@ -59,12 +86,24 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      | ------------------------------------------------------------------------------------------------
      */
     /**
-     * Assert Log entries
+     * Assert Log object
      *
-     * @param EntryCollection $entries
-     * @param string          $date
+     * @param  Log     $log
+     * @param  string  $date
      */
-    protected function assertLogEntries(EntryCollection $entries, $date)
+    protected function assertLog(Log $log, $date)
+    {
+        $this->assertEquals($date, $log->date);
+        $this->assertLogEntries($log->entries(), $log->date);
+    }
+
+    /**
+     * Assert Log entries object
+     *
+     * @param  LogEntryCollection  $entries
+     * @param  string              $date
+     */
+    protected function assertLogEntries(LogEntryCollection $entries, $date)
     {
         foreach ($entries as $entry) {
             $this->assertLogEntry($entry, $date);
@@ -72,19 +111,19 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     }
 
     /**
-     * Assert log entry
+     * Assert log entry object
      *
-     * @param  LogEntry $entry
-     * @param  string  $date
+     * @param  LogEntry  $entry
+     * @param  string    $date
      */
     protected function assertLogEntry(LogEntry $entry, $date)
     {
         $dt = Carbon::createFromFormat('Y-m-d', $date);
 
         $this->assertInLogLevels($entry->level);
-        $this->assertNotEmpty($entry->header);
         $this->assertInstanceOf(Carbon::class, $entry->datetime);
         $this->assertTrue($entry->datetime->isSameDay($dt));
+        $this->assertNotEmpty($entry->header);
         $this->assertNotEmpty($entry->stack);
     }
 
@@ -96,7 +135,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function assertInLogLevels($level, $message = '')
     {
-        $this->assertContains($level, $this->getLogLevels(), $message);
+        $this->assertContains($level, self::$logLevels, $message);
     }
 
     /**
@@ -125,9 +164,9 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     /**
      * Assert translated level
      *
-     * @param  string $locate
-     * @param  string $key
-     * @param  string $translatedLevel
+     * @param  string  $locate
+     * @param  string  $key
+     * @param  string  $translatedLevel
      */
     protected function assertTranslatedLevel($locate, $key, $translatedLevel)
     {
@@ -146,9 +185,47 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      *
      * @return \Arcanedev\LogViewer\Utilities\Filesystem
      */
-    protected function getFilesystem()
+    protected function filesystem()
     {
         return $this->app['log-viewer.filesystem'];
+    }
+
+    /**
+     * Get log content
+     *
+     * @param  string  $date
+     *
+     * @return string
+     *
+     * @throws \Arcanedev\LogViewer\Exceptions\FilesystemException
+     */
+    public function getLogContent($date)
+    {
+        return $this->filesystem()->read($date);
+    }
+
+    /**
+     * Get log object from fixture
+     *
+     * @param  string  $date
+     *
+     * @return Log
+     */
+    protected function getLog($date)
+    {
+        return new Log($date, $this->getLogContent($date));
+    }
+
+    /**
+     * Get random entry from a log file
+     *
+     * @param  string  $date
+     *
+     * @return mixed
+     */
+    protected function getRandomLogEntry($date)
+    {
+        return $this->getLog($date)->entries()->random(1);
     }
 
     /**
@@ -156,7 +233,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      *
      * @return array
      */
-    public function getLogLevels()
+    public static function getLogLevels()
     {
         $class = new ReflectionClass(new LogLevel);
 
@@ -172,11 +249,10 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
      */
     protected function createDummyLog($date)
     {
-        $fixtures    = __DIR__ . '/fixtures';
-        $source      = $fixtures . '/dummy.log';
-        $destination = $fixtures . "/logs/laravel-{$date}.log";
-
-        return copy($source, $destination);
+        return copy(
+            storage_path('dummy.log'),                // Source
+            storage_path("/logs/laravel-{$date}.log") // Destination
+        );
     }
 
     /**
