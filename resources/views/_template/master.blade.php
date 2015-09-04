2e10533c6ffd223a3d6ac603c0421f62e1e3187b
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-04 19:19:29 +0100

    Adding Chart js cdn

--- resources/views/_template/master.blade.php
+++ resources/views/_template/master.blade.php
@@ -93,6 +93,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/locale/{{ trans()->getLocale() }}.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.15.35/js/bootstrap-datetimepicker.min.js"></script>
     @yield('scripts')
 </body>
