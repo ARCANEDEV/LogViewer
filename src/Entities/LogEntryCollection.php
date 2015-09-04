36e98b6e4b4d156341009d49c831da07fa71d540
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-04 01:07:36 +0100

    Adding filterByLevel, stats, tree and menu methods

--- src/Entities/LogEntryCollection.php
+++ src/Entities/LogEntryCollection.php
@@ -30,4 +30,91 @@ class LogEntryCollection extends Collection
 
         return $this;
     }
+
+    /**
+     * Get filtered log entries by level
+     *
+     * @param  string  $level
+     *
+     * @return LogEntryCollection
+     */
+    public function filterByLevel($level)
+    {
+        return $this->filter(function(LogEntry $entry) use ($level) {
+            return $entry->isSameLevel($level);
+        });
+    }
+
+    /**
+     * Get log entries stats.
+     *
+     * @return array
+     */
+    public function stats()
+    {
+        $counters = $this->initStats();
+
+        $this->groupBy('level')
+            ->each(function (LogEntryCollection $entries, $level) use (&$counters) {
+                $counters[$level] = $count = $entries->count();
+                $counters['all'] += $count;
+            });
+
+        return $counters;
+    }
+
+    /**
+     * Get the log entries navigation tree.
+     *
+     * @param  bool|false  $trans
+     *
+     * @return array
+     */
+    public function tree($trans = false)
+    {
+        $tree = $this->stats();
+
+        array_walk($tree, function(&$count, $level) use ($trans) {
+            $count = [
+                'name'  => $trans ? trans("log-viewer::levels.$level") : $level,
+                'count' => $count,
+            ];
+        });
+
+        return $tree;
+    }
+
+    /**
+     * Get log entries menu.
+     *
+     * @param  bool|true  $trans
+     *
+     * @return array
+     */
+    public function menu($trans = true)
+    {
+        // TODO: Add the icons and stuff
+        return $this->tree($trans);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Init stats counters.
+     *
+     * @return array
+     */
+    private function initStats()
+    {
+        $levels = array_merge_recursive(
+            ['all'],
+            array_keys(log_viewer()->levels(true))
+        );
+
+        return array_map(function () {
+            return 0;
+        }, array_flip($levels));
+    }
 }
