b0482ad97ce6b6f285398475aa8aa7d1e4d2b342
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-05 23:43:51 +0100

    Cleaning the config file

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -43,26 +43,8 @@ return [
      |  Menu settings
      | ------------------------------------------------------------------------------------------------
      */
-    'menu'          => [
-        'icons' => [
-            'enabled' => true,
-
-            'levels' =>  [
-                /**
-                 * Font awesome >= 4.3
-                 * http://fontawesome.io/icons/
-                 */
-                'all'       => 'fa fa-fw fa-list',                 // http://fontawesome.io/icon/list/
-                'emergency' => 'fa fa-fw fa-bug',                  // http://fontawesome.io/icon/bug/
-                'alert'     => 'fa fa-fw fa-bullhorn',             // http://fontawesome.io/icon/bullhorn/
-                'critical'  => 'fa fa-fw fa-heartbeat',            // http://fontawesome.io/icon/heartbeat/
-                'error'     => 'fa fa-fw fa-times-circle',         // http://fontawesome.io/icon/times-circle/
-                'warning'   => 'fa fa-fw fa-exclamation-triangle', // http://fontawesome.io/icon/exclamation-triangle/
-                'notice'    => 'fa fa-fw fa-exclamation-circle',   // http://fontawesome.io/icon/exclamation-circle/
-                'info'      => 'fa fa-fw fa-info-circle',          // http://fontawesome.io/icon/info-circle/
-                'debug'     => 'fa fa-fw fa-life-ring',            // http://fontawesome.io/icon/life-ring/
-            ],
-        ],
+    'menu'  => [
+        'icons-enabled' => true,
     ],
 
     /* ------------------------------------------------------------------------------------------------
@@ -90,94 +72,47 @@ return [
      | ------------------------------------------------------------------------------------------------
      */
     'colors' =>  [
-        'empty'     => [
-            'background' => '#D1D1D1',
-            'font'       => "#FFF",
-        ],
-        'all'       => [
-            'background' => '#8A8A8A',
-            'font'       => "#FFF",
-        ],
-        'emergency' => [
-            'background' => "#B71C1C",
-            'font'       => "#FFF",
-        ],
-        'alert'     => [
-            'background' => "#D32F2F",
-            'font'       => "#FFF",
-        ],
-        'critical'  => [
-            'background' => '#F44336',
-            'font'       => "#FFF",
-        ],
-        'error'     => [
-            'background' => '#FF5722',
-            'font'       => "#FFF",
-        ],
-        'warning'   => [
-            'background' => '#FF9100',
-            'font'       => "#FFF",
-        ],
-        'notice'    => [
-            'background' => '#4CAF50',
-            'font'       => "#FFF",
-        ],
-        'info'      => [
-            'background' => "#1976D2",
-            'font'       => "#FFF",
-        ],
-        'debug'     => [
-            'background' => "#90CAF9",
-            'font'       => "#FFF",
-        ],
-    ],
+        'levels'    => [
 
-    /* ------------------------------------------------------------------------------------------------
-     |  Charts settings
-     | ------------------------------------------------------------------------------------------------
-     */
-    'charts'    => [
-        /**
-         * http://www.chartjs.org/
-         */
-        'levels' =>  [
             'empty'     => [
-                'color' => '#D1D1D1',
+                'background' => '#D1D1D1',
+                'font'       => "#FFF",
             ],
             'all'       => [
-                'color' => '#8A8A8A',
+                'background' => '#8A8A8A',
+                'font'       => "#FFF",
             ],
             'emergency' => [
-                'color'     => "#B71C1C",
-                'highlight' => "#FFF",
+                'background' => "#B71C1C",
+                'font'       => "#FFF",
             ],
             'alert'     => [
-                'color'     => "#D32F2F",
-                'highlight' => "#FFF",
+                'background' => "#D32F2F",
+                'font'       => "#FFF",
             ],
             'critical'  => [
-                'color'     => '#F44336',
-                'highlight' => "#FFF",
+                'background' => '#F44336',
+                'font'       => "#FFF",
             ],
             'error'     => [
-                'color'     => '#FF5722',
-                'highlight' => "#FFF",
+                'background' => '#FF5722',
+                'font'       => "#FFF",
             ],
             'warning'   => [
-                'color'     => '#FF9100',
-                'highlight' => "#FFF",
+                'background' => '#FF9100',
+                'font'       => "#FFF",
             ],
             'notice'    => [
-                'color'     => '#4CAF50',
-                'highlight' => "#FFF",
+                'background' => '#4CAF50',
+                'font'       => "#FFF",
             ],
             'info'      => [
-                'color'     => "#1976D2",
-                'highlight' => "#FFF",
+                'background' => "#1976D2",
+                'font'       => "#FFF",
             ],
             'debug'     => [
-                'color'     => "#90CAF9",
-                'highlight' => "#FFF",
+                'background' => "#90CAF9",
+                'font'       => "#FFF",
             ],
         ],
     ],
