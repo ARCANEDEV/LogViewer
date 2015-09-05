d88cf68e7fc2909df6d7af431160a6a967d1a57d
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-05 23:46:57 +0100

    Adding the LogViewer Route Registrar

---
+++ src/Http/Routes/LogViewerRoute.php
@@ -0,0 +1,26 @@
+<?php namespace Arcanedev\LogViewer\Http\Routes;
+
+use Arcanedev\Support\Laravel\RouteRegister;
+use Illuminate\Contracts\Routing\Registrar;
+
+/**
+ * Class LogViewerRoute
+ * @package Arcanedev\LogViewer\Http\Routes
+ */
+class LogViewerRoute extends RouteRegister
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function map(Registrar $router)
+    {
+        parent::map($router);
+
+        $this->group([
+
+        ], function () {
+            // TODO:  Complete route registration
+        });
+    }
+}
