bb170c0536406c41513016ff655c8c78271fa9d0
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-09 11:51:53 +0100

    Adding request helper

--- helpers.php
+++ helpers.php
@@ -62,3 +62,22 @@ if ( ! function_exists('extract_datetime')) {
         );
     }
 }
+
+if ( ! function_exists('request')) {
+    /**
+     * Get an instance of the current request or an input item from the request.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @return \Illuminate\Http\Request|string|array
+     */
+    function request($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('request');
+        }
+
+        return app('request')->input($key, $default);
+    }
+}
