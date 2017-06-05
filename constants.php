c8b7424c9111dccaff9a1b3c361adc91786a587c
Persevere Von <wu543065657@163.com>
2017-06-05 17:35:31 +0800

    Fix undefined constant GLOB_BRACE on Alpine Linux (#146)
    * Fix undefined constant GLOB_BRACE on Alpine Linux
    
    Fix undefined constant GLOB_BRACE exception on Alpine Linux.  
    
    [https://bugs.php.net/bug.php?id=72095](https://bugs.php.net/bug.php?id=72095)
    
    * Fix undefined constant GLOB_BRACE exception on Alpine Linux
    
    * Update constants.php

--- constants.php
+++ constants.php
@@ -14,3 +14,8 @@ if ( ! defined('REGEX_DATETIME_PATTERN')) {
         REGEX_DATE_PATTERN . ' ' . REGEX_TIME_PATTERN // YYYY-MM-DD HH:MM:SS
     );
 }
+
+// Fix undefined constant GLOB_BRACE exception on Alpine Linux (https://bugs.php.net/bug.php?id=72095)
+if ( ! defined('GLOB_BRACE')) {
+    define('GLOB_BRACE', 0);
+}
