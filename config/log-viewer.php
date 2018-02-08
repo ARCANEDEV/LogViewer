5c86beebafcb0543a5e2f19602ee2e705149b456
Unknown <arcanedev.maroc@gmail.com>
2018-02-08 14:17:43 +0000

    Updating docs

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -39,6 +39,7 @@ return [
      |    'bootstrap-3', 'bootstrap-4'
      |  Make your own theme by adding a folder to the views directory and specifying it here.
      */
+
     'theme'         => 'bootstrap-4',
 
     /* -----------------------------------------------------------------
@@ -59,7 +60,7 @@ return [
     /* -----------------------------------------------------------------
      |  Log entries per page
      | -----------------------------------------------------------------
-     |  This defines how many log entries are displayed per page.
+     |  This defines how many logs & entries are displayed per page.
      */
 
     'per-page'      => 30,
