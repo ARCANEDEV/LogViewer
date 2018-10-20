f122f92133ac52b5d3f69a08000ed8573aa73ff2
Sebastiaan Speck <shem.speck@gmail.com>
2018-10-20 14:10:09 +0200

    Added Dutch translations

---
+++ resources/lang/nl/show.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'log'       => [
+        'header' => 'Log',
+        'info' => 'Log info',
+        'download' => 'DOWNLOAD',
+        'delete' => 'VERWIJDER',
+        'path' => 'BESTANDSPAD',
+        'entries' => 'Log gebeurtenissen',
+        'size' => 'Grootte',
+        'created_at' => 'Gemaakt op',
+        'updated_at' => 'Geupdate op',
+        'search' => [
+            'placeholder' => 'Klik hier om te zoeken'
+        ],
+        'env' => 'ENV',
+        'level' => 'Level',
+        'time' => 'Tijd',
+        'table_header' => 'Header',
+        'actions' => 'Acties',
+        'stack' => 'Stack'
+
+    ],
+    'modals' => [
+        'delete' => [
+            'header' => 'VERWIJDER LOG BESTAND',
+            'close' => 'Sluiten',
+            'cancel' => 'Annuleren',
+            'submit' => 'VERWIJDER BESTAND',
+            'confirm' => 'Weet je zeker dat je dit log bestand <span class="label label-primary">\' + date + \'</span> wilt <span class="label label-danger">VERWIJDEREN</span>  ?',
+            'error' => 'AJAX ERROR ! Check de console !'
+        ]
+    ],
+    'page' => [
+        'page' => 'Pagina',
+        'of' => 'van',
+    ],
+    'levels'    => 'Levels'
+];
