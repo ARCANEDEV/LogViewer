fb9d1ac1ad6c0616c6c0e244c2cb42286554643a
ARCANEDEV <arcanedev.maroc@gmail.com>
2019-03-19 19:28:51 +0100

    Bump version

--- src/LogViewer.php
+++ src/LogViewer.php
@@ -21,7 +21,7 @@ class LogViewer implements LogViewerContract
     /**
      * LogViewer Version
      */
-    const VERSION = '4.6.3';
+    const VERSION = '4.7.1';
 
     /* -----------------------------------------------------------------
      |  Properties
